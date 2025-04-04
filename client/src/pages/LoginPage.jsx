d0c62fc16501121135529d77b094f9d666d00f25
Evgeniy Kadolich <zhenia.kadolich@gmail.com>
2025-04-04 18:57:36 +0300

    Стилизация страниц (регистрация и вход)

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { TextField, Button, Container, Typography, Divider, Link } from "@mui/material";
+import { TextField, Button, Container, Typography, Box, Link } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Background from '../static/images/bg-img.jpg';
+import Logo from '../static/images/Logo-reg-img.svg';
+document.body.style.margin = "0";
 
 const LoginPage = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
-
   const onSubmit = async (data) => {
     try {
       const res = await axios.post("http://localhost:5000/api/auth/login", data);
@@ -19,32 +21,72 @@ const LoginPage = () => {
   };
 
   return (
-    <Container maxWidth="xs" >
-      <Typography variant="h6" align="center" gutterBottom>
-        Войти в приложение
-      </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField {...register("email")} label="Email" fullWidth margin="normal" required />
-        <TextField {...register("password")} label="Пароль" type="password" fullWidth margin="normal" required />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Войти
-        </Button>
-      </form>
-      <Divider>
-            <Typography sx={{ color: 'text.secondary', padding: '5 0 5 0' }}>или</Typography>
-          </Divider>
-          <Typography sx={{ textAlign: 'center' }}>
-              Не зарегистрированы?{' '}
-              <Link
-                href="/register"
-                variant="body2"
-                sx={{ alignSelf: 'center' }}
-                underline="none"
-              >
-                Создать аккаунт
-              </Link>
-            </Typography>
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+        backgroundImage: `url(${Background})`
+      }}
+    >
+      <Container maxWidth="xs" sx={{ backgroundColor: "white", borderRadius: "15px", py: 10 }}>
+        <Box
+        sx={{
+          width: 66,
+          height: 66,
+          mx: "auto",
+          mb: 2,
+          backgroundImage: `url(${Logo})`
+        }}
+        />
+        <Typography variant="h6" align="center" gutterBottom sx={{ color: "#2e1a5a", mb: "2rem", fontFamily: 'Manrope', fontWeight: "800", fontSize: "20px" }}>
+          Войти в приложение
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register("email")}
+            label="Электронный адрес"
+            fullWidth
+            required
+            margin="dense"
+            sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px" } }}
+          />
+          <TextField
+            {...register("password")}
+            label="Пароль"
+            type="password"
+            fullWidth
+            required
+            margin="dense"
+            sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px", fontFamily: 'Manrope' } }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ backgroundColor: "#252525", color: "white", border: "1px solid", borderColor: "#B7B7B7", boxShadow: "none", borderRadius: '25px', mt: "20px", fontFamily: 'Manrope', fontWeight: "700", fontSize: "small" }}
+          >
+            Войти
+          </Button>
+        </form>
+        <Typography sx={{ textAlign: "center", marginTop: 2, fontSize: 15 }}>
+          Не зарегистрированы?{" "}
+          <Link
+            href="/register"
+            variant="body2"
+            sx={{ alignSelf: "center" }}
+            underline="none"
+            color="#E1A3FF"
+            fontSize={15}
+          >
+            Создать аккаунт
+          </Link>
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { TextField, Button, Container, Typography } from "@mui/material";
+import { TextField, Button, Container, Typography, Box, Link, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Background from '../static/images/bg-img.jpg';
+import Logo from '../static/images/Logo-reg-img.svg';
+
+document.body.style.margin = "0";
 
 const RegisterPage = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
   const onSubmit = async (data) => {
     try {
@@ -18,20 +28,103 @@ const RegisterPage = () => {
   };
 
   return (
-    <Container maxWidth="xs">
-      <Typography variant="h4" align="center" gutterBottom>
-        Регистрация
-      </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField {...register("lastName")} label="Фамилия" fullWidth margin="normal" required />
-        <TextField {...register("firstName")} label="Имя" fullWidth margin="normal" required />
-        <TextField {...register("email")} label="Email" fullWidth margin="normal" required />
-        <TextField {...register("password")} label="Пароль" type="password" fullWidth margin="normal" required />
-        <Button type="submit" variant="contained" color="primary" fullWidth>
-          Зарегистрироваться
-        </Button>
-      </form>
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+        backgroundImage: `url(${Background})`
+      }}
+    >
+      <Container maxWidth="sm" sx={{ backgroundColor: "white", borderRadius: "15px", py: 10, px: 4 }}>
+        <Box
+          sx={{
+            width: 66,
+            height: 66,
+            mx: "auto",
+            mb: 2,
+            backgroundImage: `url(${Logo})`
+          }}
+        />
+        <Typography variant="h6" align="center" gutterBottom sx={{ color: "#2e1a5a", mb: "2rem", fontFamily: 'Manrope', fontWeight: "800", fontSize: "20px" }}>
+          Зарегистрироваться в приложении
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box sx={{ display: 'flex', gap: 2, mb: 1 }}>
+            <TextField
+              {...register("firstName")}
+              label="Имя"
+              fullWidth
+              required
+              margin="dense"
+              sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px" } }}
+            />
+            <TextField
+              {...register("lastName")}
+              label="Фамилия"
+              fullWidth
+              required
+              margin="dense"
+              sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px" } }}
+            />
+          </Box>
+          <TextField
+            {...register("email")}
+            label="Электронный адрес"
+            fullWidth
+            required
+            margin="dense"
+            sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px" } }}
+          />
+          <TextField
+            {...register("password")}
+            label="Пароль"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            required
+            margin="dense"
+            sx={{ "& .MuiOutlinedInput-notchedOutline": { borderColor: "#B7B7B7", borderRadius: "20px", fontFamily: 'Manrope' } }}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ backgroundColor: "#252525", color: "white", border: "1px solid", borderColor: "#B7B7B7", boxShadow: "none", borderRadius: '25px', mt: "20px", fontFamily: 'Manrope', fontWeight: "700", fontSize: "small" }}
+          >
+            Зарегистрироваться
+          </Button>
+        </form>
+        <Typography sx={{ textAlign: "center", marginTop: 2, fontSize: 15 }}>
+          Есть аккаунт?{" "}
+          <Link
+            href="/login"
+            variant="body2"
+            sx={{ alignSelf: "center" }}
+            underline="none"
+            color="#E1A3FF"
+            fontSize={15}
+          >
+            Войти
+          </Link>
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
